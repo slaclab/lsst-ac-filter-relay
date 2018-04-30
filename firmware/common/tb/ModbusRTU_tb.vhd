f5217adfc782ff79725f41563f0ae7bc8a62e5f6
vanxiong <vanxiong@slac.stanford.edu>
2018-04-30 09:31:25 -0700

    Changed state machine logic

--- firmware/common/tb/ModbusRTU_tb.vhd
+++ firmware/common/tb/ModbusRTU_tb.vhd
@@ -10,77 +10,156 @@ end ModbusRTU_tb;
 
 architecture behavior of ModbusRTU_tb is
   --entity instantiation--
-  
+
   --signals
-  signal clk : sl   := '0';
-  signal rst : sl   := '0';
+  signal clk : sl := '0';
+  signal rst : sl := '1';
 
   -- Transmit parallel interface
-  signal wrData     : slv(47 downto 0) := x"0000_0000_0000"; --: in  slv(47 downto 0);
-  signal wrValid    : sl := '0';            --: in  sl;
-  signal wrReady    : sl := '0';            --: out sl;
+  signal wrData  : slv(47 downto 0);    --: in  slv(47 downto 0);
+  signal wrValid : sl;                  --: in  sl;
+  signal wrReady : sl;                  --: out sl;
   -- Receive parallel interface
-  signal rdData     : slv(63 downto 0) := x"0000_0000_0000_0000";          --: out slv(63 downto 0);
-  signal rdValid    : sl := '0';            --: out sl;
-  signal rdReady    : sl := '0';            --: in  sl;
-  
+  signal rdData  : slv(63 downto 0);    --: out slv(63 downto 0);
+  signal rdValid : sl;                  --: out sl;
+  signal rdReady : sl;                  --: in  sl;
+
 -- SN65HVD1780QDRQ1 interface (RS485 transceiver) --
-  signal rx         : sl := '0';            --: in sl; 
-  signal rx_En      : sl := '0';            --: out sl; --needs to be same as tx_En
-  signal tx         : sl := '0';            --: out sl; 
-  signal tx_En      : sl := '0';            --: out sl  --needs to be same as rx_EN
-  
+  signal rx    : sl;                    --: in sl; 
+  signal rx_En : sl;  --: out sl; --needs to be same as tx_En
+  signal tx    : sl;                    --: out sl; 
+  signal tx_En : sl;  --: out sl  --needs to be same as rx_EN
+
+  signal wrNotValid : sl := '0';
+  signal randomstuff : sl := '0';
 
 begin
 
-U_ModbusRTU : entity work.ModbusRTU
-        generic map (
-           TPD_G             => 1 ns
-           )
-        port map (
-           clk     => clk,           -- [in]
-           rst     => rst,           -- [in]
--- SN65HVD1780QDRQ1 interface (RS485 transceiver) --
-           rx      => rx,            --[in]
-           rx_En   => rx_En,         --[out]
-           tx      => tx,            --[out]
-           tx_En   => tx_En,         --[out]
-           
--- Mobus Data --    
-           wrData     => wrData,     --[in]
-           
-           wrValid    => wrValid,    --[in]    --- still need to work on this
-           rdReady    => '1',        --[in]    --- still need to work on this
-           
-           rdData  =>  rdData,      --[out]
-           rdValid =>  rdValid      --[out]
-           );
+  U_ModbusRTU : entity work.ModbusRTU
+    generic map (
+      TPD_G => 1 ns
+      )
+    port map (
+      clk        => clk,                -- [in]
+      rst        => rst,                -- [in]
+      -- SN65HVD1780QDRQ1 interface (RS485 transceiver) --
+      rx         => rx,                 --[in]
+      rx_En      => rx_En,              --[out]
+      tx         => tx,                 --[out]
+      tx_En      => tx_En,              --[out]        
+      -- Mobus Data --    
+      wrData     => wrData,             --[in]
+      wrValid    => wrValid,  --[in]    --- still need to work on this
+      wrNotValid => wrNotValid,         --[out]
+      rdReady    => '1',  --[in]    --- still need to work on this
+      rdData     => rdData,             --[out]
+      rdValid    => rdValid             --[out]
+      );
 
 
-
+  
   Clk_process : process
   begin
-    clk <= '0';
-    wait for 4 ns;
-    clk <= '1';
+    clk <= not(clk);
     wait for 4 ns;
   end process;
-  
---  Rst_process : process
---  begin
---    rst <= '0';
---    wait for 100000 ns;
---    rst <= '1';
---    wait for 4 ns;
---  end process;
-  
-  Data_process : process
+
+  Rst_process : process
   begin
-    wait for 3 ns;
-    wrData <= x"01f1_1010_0100";
+    rst <= '1';
+    wait for 100 us;
+	rst <= '0';
+	wait;
+  end process;
+
+  TxData_process : process
+  begin
+    wait for 200 us;
     wrValid <= '1';
---    wait for 400 ns;
---    wrValid <= '0';
+    wrData  <= x"ffff_ffff_ffff";
+	wait for 2 ms;
+    wrValid <= '0';
+    wait;
+  end process;
+  
+  RxData_process : process
+  begin
+    wait for 1300 us;
+	--1
+    rx  <= '1';
+	wait for 9240 ns;
+	rx  <= '1';
+	wait for 9240 ns;
+	rx  <= '1';
+	wait for 9240 ns;
+	rx  <= '0';
+	--2
+	wait for 9240 ns;
+	rx  <= '1';
+	wait for 9240 ns;
+	rx  <= '0';
+	wait for 9240 ns;
+	rx  <= '1';
+	wait for 9240 ns;
+	rx  <= '0';
+	--3
+	wait for 9240 ns;
+	rx  <= '1';
+	wait for 9240 ns;
+	rx  <= '0';
+	wait for 9240 ns;
+	rx  <= '1';
+	wait for 9240 ns;
+	rx  <= '0';
+	--4
+	wait for 9240 ns;
+	rx  <= '1';
+	wait for 9240 ns;
+	rx  <= '0';
+	wait for 9240 ns;
+	rx  <= '1';
+	wait for 9240 ns;
+	rx  <= '0';
+	--5
+    wait for 1300 us;
+    rx  <= '1';
+	wait for 9240 ns;
+	rx  <= '1';
+	wait for 9240 ns;
+	rx  <= '1';
+	wait for 9240 ns;
+	rx  <= '0';
+	--6
+	wait for 9240 ns;
+	rx  <= '1';
+	wait for 9240 ns;
+	rx  <= '0';
+	wait for 9240 ns;
+	rx  <= '1';
+	wait for 9240 ns;
+	rx  <= '0';
+	--7
+	wait for 9240 ns;
+	rx  <= '1';
+	wait for 9240 ns;
+	rx  <= '0';
+	wait for 9240 ns;
+	rx  <= '1';
+	wait for 9240 ns;
+	rx  <= '0';
+	--8
+
+	wait for 9240 ns;
+	rx  <= '1';
+	wait for 9240 ns;
+    rx  <= '1';
+	wait for 9240 ns;
+    rx  <= '1';
+	wait for 9240 ns;
+    rx  <= '1';
+	
+	wait for 9240 ns;
+    rx  <= '0';
     wait;
   end process;
 
